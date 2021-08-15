c13f6342b6f4674173dca38d105c2f994d97dc2a
Brett Holland <bhdevelop@outlook.com>
2021-08-15 12:45:35 -0400

    Began first draft of the data extract and conversion class.

--- CycleLibrary/ProcessData.cs
+++ CycleLibrary/ProcessData.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using CycleLibrary.DataClasses;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CycleLibrary
 {
-    public static class ProcessData
+    public class ProcessData
     {
-        public static Dictionary<int, string> weather = new Dictionary<int, string>()
+        private Dictionary<int, string> _weather = new Dictionary<int, string>()
         {
             { 0,"Sunny" },
             { 1,"Little Cloudy" },
@@ -15,6 +16,32 @@ namespace CycleLibrary
             { 4,"Snowy" },
             { 5,"Stormy" }
         };
-        
+
+        private bool _csvExport = false;
+        private bool _gpxExport = false;
+        private bool _tcxExport = false;
+
+        private List<TripDetails> _trips;
+
+        private string _baseFolderPath;
+        private string _xmlFolderPath;
+        private string _csvFolderPath;
+        private string _gpxFolderPath;
+        private string _tcxFolderPath;
+
+
+        public ProcessData(List<TripDetails> trips, bool csvExport, bool gpxExport, bool tcxExport)
+        {
+            _trips = trips;
+
+            _csvExport = csvExport;
+            _gpxExport = gpxExport;
+            _tcxExport = tcxExport;
+        }
+
+        public void AssignBaseFolderPath(string baseFolderPath)
+        {
+
+        }
     }
 }
