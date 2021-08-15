5c7f81eab090aec867214169aad5bb46f85b56cd
Brett Holland <bhdevelop@outlook.com>
2021-08-15 11:13:32 -0400

    Added local time variables to preserve that original data. Added UTC string variables needed for GPX and TCX.

--- CycleLibrary/XmlFile.cs
+++ CycleLibrary/XmlFile.cs
@@ -14,6 +14,26 @@ namespace CycleLibrary
         public Metadata RideMetadata { get; set; }
 
         public List<TrackSegment> RideTracks { get; set; }
+
+        public string GetFileName(string extensionNoDot)
+        {
+            string output;
+
+            if (extensionNoDot.StartsWith("."))
+            {
+                output = extensionNoDot.Substring(1);
+            }
+            else
+            {
+                output = extensionNoDot;
+            }
+
+            output = output.Trim();
+
+            output = $"{RideMetadata.RideID}.{output}";
+
+            return output;
+        }
     }
 
 
@@ -28,10 +48,16 @@ namespace CycleLibrary
         public string RideID { get; set; }
         public string Title { get; set; }
         public double Distance { get; set; }
+        public DateTime StartTimeLocal { get; set; }
         public DateTime StartTimeUtc { get; set; }
+        public string StartTimeUtcString { get; set; }
+        public DateTime EndTimeLocal { get; set; }
         public DateTime EndTimeUtc { get; set; }
+        public string EndTimeUtcString { get; set; }
         public int DurationSeconds { get; set; }
+        public DateTime RideDateTimeLocal { get; set; }
         public DateTime RideDateTimeUtc { get; set; }
+        public string RideDateTimeUtcString { get; set; }
         public int TotalCaloriesBurned { get; set; }
         public double MaxSpeed { get; set; }
         public string Weather { get; set; }
@@ -58,11 +84,12 @@ namespace CycleLibrary
         }
 
 
+        public DateTime PointTimeLocal { get; set; }
         public DateTime PointTimeUtc { get; set; }
+        public string PointTimeUtcString { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
         public double Altitude { get; set; }
-        // CONVERT THE ACCURACTY IN THE SERIALIZATION CLASS FROM INT TO DOUBLE
         public double HorizontalAccuracy { get; set; }
         public double VerticalAccuracy { get; set; }
         public double Speed { get; set; }
