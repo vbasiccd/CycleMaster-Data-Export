c44b45c1732c63be64c301192fa0449ef0506b76
Brett Holland <bhdevelop@outlook.com>
2021-08-15 21:05:38 -0400

    Added the WriteXmlFile method.

--- CycleLibrary/FileIO.cs
+++ CycleLibrary/FileIO.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+using System.Xml;
 
 namespace CycleLibrary
 {
-    class FileIO
+    public static class FileIO
     {
+        public static void WriteXmlFile(ref XmlDocument xmlDocument, string filePath)
+        {
+            using (TextWriter fileWriter = new StreamWriter(filePath))
+            {
+                xmlDocument.Save(fileWriter);
+            }
+        }
     }
 }
